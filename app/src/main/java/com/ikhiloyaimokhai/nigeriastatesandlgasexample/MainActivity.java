19797b07b3eb44521efde0101851fbc997918dd7
Ikhiloya <iimokhai@ecobank.com>
2019-05-15 08:52:50 +0100

    reduced height of spinner and made spinners scrollable

--- app/src/main/java/com/ikhiloyaimokhai/nigeriastatesandlgasexample/MainActivity.java
+++ app/src/main/java/com/ikhiloyaimokhai/nigeriastatesandlgasexample/MainActivity.java
@@ -2,12 +2,124 @@ package com.ikhiloyaimokhai.nigeriastatesandlgasexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.ikhiloyaimokhai.nigeriastatesandlgas.Nigeria;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private Spinner mStateSpinner, mLgaSpinner;
+    private String mState, mLga;
+    private List<String> states;
+    private static final int SPINNER_HEIGHT = 500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStateSpinner = findViewById(R.id.stateSpinner);
+        mLgaSpinner = findViewById(R.id.lgaSpinner);
+        resizeSpinner(mStateSpinner, SPINNER_HEIGHT);
+        resizeSpinner(mLgaSpinner, SPINNER_HEIGHT);
+
+        states = Nigeria.getStates();
+
+        //call to method that'll set up state and lga spinner
+        setupSpinners();
+
+    }
+
+
+    /**
+     * Method to set up the spinners
+     */
+    public void setupSpinners() {
+        // Create adapter for spinner. The list options are from the String array it will use
+        // the spinner will use the default layout
+        //populates the quantity spinner ArrayList
+
+        ArrayAdapter<String> statesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, states);
+
+        // Specify dropdown layout style - simple list view with 1 item per line
+        statesAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        // Apply the adapter to the spinner
+        statesAdapter.notifyDataSetChanged();
+        mStateSpinner.setAdapter(statesAdapter);
+
+        // Set the integer mSelected to the constant values
+        mStateSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mState = (String) parent.getItemAtPosition(position);
+                setUpStatesSpinner(position);
+            }
+
+            // Because AdapterView is an abstract class, onNothingSelected must be defined
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Unknown
+            }
+        });
+    }
+
+
+    /**
+     * method to set up the state spinner
+     *
+     * @param position current position of the spinner
+     */
+    private void setUpStatesSpinner(int position) {
+        List<String> list = new ArrayList<>(Nigeria.getLgasByState(states.get(position)));
+        setUpLgaSpinner(list);
+    }
+
+
+    /**
+     * Method to set up the local government areas corresponding to selected states
+     *
+     * @param lgas represents the local government areas of the selected state
+     */
+    private void setUpLgaSpinner(List<String> lgas) {
+
+        ArrayAdapter lgaAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, lgas);
+        lgaAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        lgaAdapter.notifyDataSetChanged();
+        mLgaSpinner.setAdapter(lgaAdapter);
+
+        mLgaSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+                mLga = (String) parent.getItemAtPosition(position);
+                Toast.makeText(MainActivity.this, "state: " + mState + " lga: " + mLga , Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+    }
+
+    private void resizeSpinner(Spinner spinner, int height) {
+        try {
+            Field popup = Spinner.class.getDeclaredField("mPopup");
+            popup.setAccessible(true);
+
+            //Get private mPopup member variable and try cast to ListPopupWindow
+            android.widget.ListPopupWindow popupWindow = (android.widget.ListPopupWindow) popup.get(spinner);
+
+            //set popupWindow height to height
+            popupWindow.setHeight(height);
+
+        } catch (NoClassDefFoundError | ClassCastException | NoSuchFieldException | IllegalAccessException ex) {
+            ex.printStackTrace();
+        }
     }
 }
